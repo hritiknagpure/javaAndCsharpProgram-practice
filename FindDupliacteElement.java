d0c15f227e8328930d07c5540467fc5bbf782fba
Hritiknagpure <79217061+hritiknagpure@users.noreply.github.com>
2024-03-14 23:46:17 +0530

    Create FindDupliacteElement.java

---
+++ FindDupliacteElement.java
@@ -0,0 +1,24 @@
+public class FindDupliacteValue {
+  public static void main(String[] args) {
+
+    int arr[] = {
+      1,
+      2,
+      2,
+      3,
+      4,
+      4,
+      5,
+      6
+    };
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = i + 1; j < arr.length; j++) {
+        if (arr[i] == arr[j]) {
+          System.out.println(arr[i]);
+        }
+      }
+    }
+
+  }
+
+}
