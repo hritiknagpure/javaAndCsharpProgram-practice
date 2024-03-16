dca00b830490ef9d994126e198c3a17e1e09cfe9
Hritiknagpure <79217061+hritiknagpure@users.noreply.github.com>
2024-03-16 10:31:03 +0530

    Create ArrayElementAddition.java

---
+++ ArrayElementAddition.java
@@ -0,0 +1,11 @@
+public class Solution
+{
+	public static void main(String[] args) {
+	    int sum=0;
+	    int arr[]={5,5,2,6,2,5,6,9}; //40
+	    for(int i=0;i<arr.length;i++){
+	        sum=sum+arr[i];
+	    }
+	    System.out.print("The addition for arr element: "+sum);
+	}
+}
