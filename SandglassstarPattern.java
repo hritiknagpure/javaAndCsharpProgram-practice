20bccf5c4fd0a66026a5321074a327457da25109
Hritiknagpure <79217061+hritiknagpure@users.noreply.github.com>
2024-03-22 23:00:31 +0530

    SandglassstarPattern.java

---
+++ SandglassstarPattern.java
@@ -0,0 +1,31 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		for (int i = 1; i <= 6; i++ ) 
+		{
+			for (int k = 1; k < i; k++ ) 
+			{
+				System.out.print(" ");
+			}
+			for(int j = i; j <= 6; j++) 
+			{
+				System.out.print(" *");
+			}
+			System.out.println();
+		}
+		
+		for (int i = 6 - 1; i >= 1; i-- ) 
+		{
+			for (int k = 1; k < i; k++ ) 
+			{
+				System.out.print(" ");
+			}
+			for(int j = i; j <= 6; j++) 
+			{
+				System.out.print(" *");
+			}
+			System.out.println();
+		}
+	}
+}
