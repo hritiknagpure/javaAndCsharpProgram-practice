6fee1eb6b94b0265a00c137e3a23e85bc4926be0
Hritiknagpure <79217061+hritiknagpure@users.noreply.github.com>
2024-04-01 22:14:43 +0530

    NivenNum.java

---
+++ NivenNum.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 18;
+        int temp = num;
+        int rem, sum = 0;
+        while (num > 0) {
+            rem = num % 10;
+            sum = sum + rem;
+            num = num / 10;
+        }
+        if (temp % sum == 0) {
+            System.out.print("Nivennumber");
+        } else {
+            System.out.print("Not a Niven number");
+        }
+    }
+}
