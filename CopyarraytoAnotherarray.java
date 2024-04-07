4efaa7e28f9c3ba5ee7d12643c2d1388e21a8b04
Hritiknagpure <79217061+hritiknagpure@users.noreply.github.com>
2024-04-07 19:38:54 +0530

    CopyarraytoAnotherarray.java

---
+++ CopyarraytoAnotherarray.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String args[]) {
+        int a[] = {1, 2, 3, 4, 5, 67};
+        int b[] = new int[a.length];
+        for (int i = 0; i < a.length; i++) {
+            b[i] = a[i];
+        }
+        for (int i = 0; i < b.length; i++) {
+            System.out.print(b[i] + " ");
+        }
+    }
+}
