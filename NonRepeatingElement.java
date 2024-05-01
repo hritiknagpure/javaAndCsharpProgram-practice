e892df0888f4ad73b50cd4781c2c3655948ffe8b
Hritiknagpure <79217061+hritiknagpure@users.noreply.github.com>
2024-05-01 14:52:59 +0530

    NonRepeatingElement.java

---
+++ NonRepeatingElement.java
@@ -0,0 +1,20 @@
+public class NonRepeatingElement {
+
+		    public static void main(String[] args) {
+		        int arr[] = {1, 2, 3,6,7, 2, 1, 3, 6};
+		        for (int i = 0; i < arr.length; i++) {
+		            int x = arr[i];
+		            int count = 0;
+		            for (int j = 0; j < arr.length; j++) {
+		                if (arr[j] == x) {
+		                    count++;
+		                }
+		            }
+		            if (count == 1) {
+		                System.out.println(arr[i]);
+		            }
+		        }
+		    }
+
+		}
+		OUTPUT_____-> 6
