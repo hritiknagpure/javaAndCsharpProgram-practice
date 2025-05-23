c0121fbb331d6a1892eb7b9322968693ed16f312
Hritiknagpure <79217061+hritiknagpure@users.noreply.github.com>
2025-05-23 12:52:50 +0530

    Create StackGeneric.cs

---
+++ C#Collections/StackGeneric.cs
@@ -0,0 +1,26 @@
+Stack follows LIFO (Last In, First Out).
+
+Use .Push() to add, .Pop() to remove, and .Peek() to view the top.
+
+It's non-generic, so items are stored as object.
+
+Use casting when retrieving specific types (not type-safe).
+
+  //code
+using System.Collections.Generic;
+using System;
+class HelloWorld {
+  static void Main() {
+      Stack<string> student=new Stack<string>();
+      student.Push("hritik");
+      student.Push("rohit");
+      student.Push("raj");
+      foreach(var item in student){
+          Console.WriteLine(item);
+      }
+  }
+}
+// output
+raj
+rohit
+hritik
